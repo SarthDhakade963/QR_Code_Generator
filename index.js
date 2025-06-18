6bbe3fb7734aaf0b9285b9762ebbb192a167175f
Sarthak <sarthakdhakde@gmail.com>
2025-06-18 16:06:50 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let urlInput = document.querySelector(".url");
+let qrImg = document.querySelector(".QRimg");
+let genBtn = document.querySelector(".generate-btn");
+let qrImgContainer = document.querySelector(".qr-img");
+
+genBtn.addEventListener("click", getQR);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getQR();
+  }
+});
+
+function getQR() {
+  const url = urlInput.value.trim();
+
+  if (!url) {
+    alert("Please enter a valid text or URL");
+    return;
+  }
+
+  qrImg.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=230x230&data=" +
+    URL.value;
+
+  qrImgContainer.style.display = "block";
+}
